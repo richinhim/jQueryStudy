788780df00efa353957fc419f4e4f1dba84652a8
richinhim <kwlsy666@naver.com>
2021-06-17 10:55:26 +0900

    스크롤 고정 완료

--- jQuery_14_sticky_header_basic_BASE/js/main.js
+++ jQuery_14_sticky_header_basic_BASE/js/main.js
@@ -1,4 +1,36 @@
 $(function () {
 
+    // 스크롤의 양- scrollTop()
+
+    // 화면상단에서 거리  - offset.top
+
+    //addClass
+    //removeClass
+    //screen reader 
+
+    var wind = $(window),
+        header = $('.page-header'),
+        headerOffsetTop = header.offset().top;
+
+    console.log(headerOffsetTop);    
+
+    // wind.on('scroll', function(){
+
+    // });
+
+    wind.scroll(function(){
+
+        // if(wind.scrollTop() >= headerOffsetTop){
+        if($(this).scrollTop() >= headerOffsetTop){
+
+            header.addClass('sticky');
+
+        }else{
+
+            header.removeClass('sticky');
+        }
+
+    });
+
 
 });

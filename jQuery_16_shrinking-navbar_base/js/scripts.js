de9bd710785185ef8b5599f41bb13c5feec6d3e2
richinhim <kwlsy666@naver.com>
2021-06-19 09:27:22 +0900

    스크롤시 헤더고정하기 - 축소되는 헤더 shrinking header

--- jQuery_16_shrinking-navbar_base/js/scripts.js
+++ jQuery_16_shrinking-navbar_base/js/scripts.js
@@ -4,7 +4,47 @@
 * ---------------------------------------------------------------------------
 */
 
+var $window = $(window),
+    $mainHeader = $('#main-header'),
+    $defaultLogo = 'images/logo.svg',
+    $smallLogo =  'images/logo-shrink.svg';
 
+    $window.scroll(function(){
+
+        if($(this).scrollTop() > 100){
+            if(!$mainHeader.hasClass('shrink')){
+
+                $mainHeader.addClass('shrink');
+                switchImages($smallLogo);
+            }
+        }else{
+
+            if($mainHeader.hasClass('shrink')){
+
+                $mainHeader.removeClass('shrink');
+                switchImages($defaultLogo);
+            }
+        }
+    });
+
+    //switchImages 함수
+
+    function switchImages(newPath){
+
+        var $logo =  $('#logo');
+
+        $logo.fadeOut(300, function(){
+
+            //var c = A.attr('src') ;
+            // A.attr('src', 'c');
+            $logo.attr('src', newPath);
+
+            $logo.fadeIn(300);
+
+        }); //jquery fadeout  https://api.jquery.com/fadeout/
+
+
+    }
 
 /*
 * ---------------------------------------------------------------------------

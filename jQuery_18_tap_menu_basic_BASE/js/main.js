6f3a379a1691d88c4b1fa4a62e28f3261a3fc5ca
richinhim <kwlsy666@naver.com>
2021-06-21 16:16:15 +0900

    tab2 완료

--- jQuery_18_tap_menu_basic_BASE/js/main.js
+++ jQuery_18_tap_menu_basic_BASE/js/main.js
@@ -1,18 +1,40 @@
 $(function () {
-	var tabAnchor = $('.tabs-nav  a'),
+	var tabAnchor = $('.tabs-nav li'),
 		tabPanel = $('.tabs-panel');
 		
 	tabAnchor.on('click',function(e){
+
 		e.preventDefault();
-		tabAnchor.removeClass('active');
-		$(this).addClass('active');
+
+		/*
+		tabAnchor.find('a').removeClass('active'); //모두 빼고 
+		$(this).find('a').addClass('active');      // 그요소만 추가 
+		*/
 		//var NewLink = $(this).attr('href');
 		
+		$(this).find('a').addClass('active');
+		$(this).siblings().find('a').removeClass('active');
+
 		tabPanel.hide();
-		$($(this).attr('href')).show();
+
+		var $targetIdx = $(this).index();
+
+		console.log($targetIdx); //순번 알아오기 
+
+		tabPanel.eq($targetIdx).show();
 		
+		/*
+		var $target = $(this).attr('href');
+
+		console.log($target);
+
+		$($target).show();
+		//$($(this).attr('href')).show();
+		*/
+
 	});
 	
+	//.trigger 방아쇠를 당기다 
 	tabAnchor.eq(0).trigger('click');		
 
 });
